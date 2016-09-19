2cae0abef07c40990b3c70aa138a6ae1ae9bca4e
Игорь <rusenko.i@ya.ru>
2016-09-19 20:40:18 +0300

    0.3

--- YandexMaps.php
+++ YandexMaps.php
@@ -38,35 +38,42 @@ class YandexMaps extends \yii\base\Widget{
         $js = NULL;
         $view = $this->getView();
         YandexMapsAsset::register($view);
-        
+
         $map_options = [];
-        foreach ($this->_options['map'] as $k=>$v){if(!empty($v)){$map_options[] = $k.':'.(is_array($v)?'['.implode(',',$v).']':'"'.$v.'"');}}
-        $js .= 'myMap = new ymaps.Map("'.$this->_options['id'].'",{'.implode(',',$map_options).'})';
-        if(!empty($this->_options['clusters'])){
-            $js .= ',clusterer = new ymaps.Clusterer({preset:"islands#invertedVioletClusterIcons",groupByCoordinates: false,clusterDisableClickZoom: true,clusterHideIconOnBalloonOpen: false,geoObjectHideIconOnBalloonOpen: false}),geoObjects = []';
+        foreach ($this->_options['map'] as $k=>$v){
+            if($k=='controls' and $v===FALSE){$v = [];}
+            $map_options[] = $k.':'.(is_array($v)?'['.implode(',',$v).']':'"'.$v.'"');
         }
-        $js .= ';';
-        if(!empty($this->_options['placemarks']) OR $this->_options['placemarks']!==FALSE){
-            $placemark = 'new ymaps.Placemark(%s,%s,%s);';
-            if(is_array($this->_options['placemarks']) and count($this->_options['placemarks'])>0){
-                $placemark = sprintf($placemark,'(item.coords?(item.coords instanceof Array?item.coords:item.coords.split(",")):myMap.getCenter())','((item.hint || item.ballon)?{hintContent:item.hint,balloonContent:item.ballon}:{})','item.hint','item.ballon','item.icon');
-                $js .= 'var placemarks = '.\yii\helpers\Json::encode($this->placemarks).',geo = myMap.geoObjects;
-                    placemarks.forEach(function(item,i){'.(!empty($this->_options['clusters'])?'geoObjects[i] = '.$placemark:'geo.add('.$placemark.')').';});';
-            }
-            else{
-                $placemark = sprintf($placemark,'myMap.getCenter()','""','""','""');
-                $js .= !empty($this->_options['clusters'])?'geoObjects[] = '.$placemark:'myMap.geoObjects.add('.$placemark.')';
-            }
-        }
-        $js .= 'console.log(geoObjects);';
-        if(!empty($this->_options['clusters'])){
-            $js .= 'clusterer.options.set({gridSize: 80,clusterDisableClickZoom: true});
+        
+        ob_start();
+        ?>
+        myMap = new ymaps.Map("<?=$this->_options['id']?>",{<?=implode(',',$map_options)?>})<?if(!empty($this->_options['clusters'])){?>,clusterer = new ymaps.Clusterer(),geoObjects = []<?}else{?>,geoCollection = new ymaps.GeoObjectCollection()<?}?>;
+        <?if(!empty($this->_options['placemarks']) OR $this->_options['placemarks']!==FALSE){?>
+            <?if(is_array($this->_options['placemarks']) and count($this->_options['placemarks'])>0){?>
+                var placemarks = <?=\yii\helpers\Json::encode($this->_options['placemarks'])?>;
+                placemarks.forEach(function(item,i){
+                    pm = new ymaps.Placemark(
+                        (item.coords?(item.coords instanceof Array?item.coords:item.coords.split(",")):myMap.getCenter()),
+                        ((item.hint || item.ballon)?{hintContent:item.hint,balloonContent:item.ballon}:{}),
+                        (item.icon?item.icon:{})
+                    );
+                    <?if(!empty($this->_options['clusters'])){?>geoObjects.push(pm);<?}else{?>geoCollection.add(pm);<?}?>
+                });
+            <?}else{?>
+                pm = new ymaps.Placemark(myMap.getCenter());
+                <?if(!empty($this->_options['clusters'])){?>geoObjects.push(pm);<?}else{?>geoCollection.add(pm);<?}?>
+            <?}}?>
+            <?if(!empty($this->_options['clusters'])){?>
                 clusterer.add(geoObjects);
-                myMap.geoObjects.add(geoObjects);';
-        }
-        $js .= 'myMap.setBounds(clusterer.getBounds(), {
-            checkZoomRange: true
-        });';
+                myMap.geoObjects.add(clusterer);
+                myMap.setBounds(clusterer.getBounds(),{checkZoomRange: true});
+            <?}else{?>
+                myMap.geoObjects.add(geoCollection);	
+                myMap.setBounds(geoCollection.getBounds());
+            <?}?>
+        <?
+        $js = ob_get_contents();
+        ob_end_clean();
         $view->registerJs('ymaps.ready(function(){'.$js.'});',\yii\web\View::POS_END);
     }
 }
